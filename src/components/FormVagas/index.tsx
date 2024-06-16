b8991fd9e33c3341dd30bf0d7f1ffe2c554a4d95
Julia Alvim <juliameuportoseguro@gmail.com>
2024-06-16 17:35:21 -0300

    projeto finalizado

---
+++ src/components/FormVagas/index.tsx
@@ -0,0 +1,28 @@
+import { FormEvent, useState } from 'react'
+
+import { Form, BotaoPesquisar, Campo } from './styles'
+
+type Props = {
+  aoPesquisar: (termo: string) => void
+}
+
+const FormVagas = ({ aoPesquisar }: Props) => {
+  const [termo, setTermo] = useState<string>('')
+
+  const aoEnviarForm = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    aoPesquisar(termo.toLocaleLowerCase())
+  }
+
+  return (
+    <Form onSubmit={aoEnviarForm}>
+      <Campo
+        placeholder="Front-end, fullstack, node, design"
+        onChange={(e) => setTermo(e.target.value)}
+        type="search"
+      />
+      <BotaoPesquisar type="submit"> Pesquisar </BotaoPesquisar>
+    </Form>
+  )
+}
+export default FormVagas
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,14 @@
+import { Form, Container, Titulo } from './styles'
+
+const Hero = () => (
+  <Form>
+    <Container className="container">
+      <Titulo>
+        {' '}
+        As melhores vagas para tecnologia, design e artes visuais.
+      </Titulo>
+    </Container>
+  </Form>
+)
+
+export default Hero
